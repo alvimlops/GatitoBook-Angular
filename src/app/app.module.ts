369e4b859771df8e8fa155c9fac7f35f1c6453b6
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-11 14:59:57 -0300

    validação de compos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MensagemModule } from './componentes/mensagem/mensagem.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MensagemModule],
   providers: [],
   bootstrap: [AppComponent]
 })
