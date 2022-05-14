bb49d1f3cf8cbc8994ab5d594d7f411f11e9e09a
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-14 20:26:31 -0300

    Conclusão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
 import { MensagemModule } from './componentes/mensagem/mensagem.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
@@ -9,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MensagemModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MensagemModule, CabecalhoModule, RodapeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
