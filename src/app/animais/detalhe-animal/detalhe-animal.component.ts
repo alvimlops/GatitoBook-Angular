a3c28f36204bc56ced3c7c141b35d951c60d7bd3
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-21 17:19:41 -0300

    Curtir e Excluir Foto

--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -2,7 +2,7 @@ import { AnimaisService } from './../animais.service';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Animal } from '../animais';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
@@ -14,11 +14,26 @@ export class DetalheAnimalComponent implements OnInit {
   animalID!: number;
   animal$: Observable<Animal> | undefined;
 
-  constructor(private animalService: AnimaisService, private activatedRoute:ActivatedRoute) { }
+  constructor(private animalService: AnimaisService, private activatedRoute:ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.animalID = this.activatedRoute.snapshot.params['animalId'];
-    this.animalID = this.animalService.buscaPorID(this.animalID);
+    this.animal$ = this.animalService.buscaPorID(this.animalID);
   }
 
+  curtir(){
+    this.animalService.curtir(this.animalID).subscribe((curtida)=>{
+      if(curtida){
+        this.animal$ = this.animalService.buscaPorID(this.animalID);
+      }
+    })
+
+  }
+
+  excluir(){
+this.animalService.excluirAnimal(this.animalID).subscribe(()=>{
+  this.router.navigate(['/animais/']);
+}, (error)=> console.log(error))
+
+  }
 }
