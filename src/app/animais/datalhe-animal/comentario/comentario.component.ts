05c6ba55f90f915ae4847da361bdd9903a311cc4
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-22 00:37:01 -0300

    componente

--- src/app/animais/datalhe-animal/comentario/comentario.component.ts
+++ src/app/animais/datalhe-animal/comentario/comentario.component.ts
@@ -1,6 +1,9 @@
-import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable, tap } from 'rxjs';
 import { Comentario, Comentarios } from './../../detalhe-animal/comentario/comentario';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ComentarioService } from '../../detalhe-animal/comentario/comentario.service';
 
 @Component({
   selector: 'app-comentario',
@@ -8,11 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comentario.component.css']
 })
 export class ComentarioComponent implements OnInit {
+  @Input() id!: number;
   Comentarios$!: Observable<Comentarios>;
+  ComentarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(private comentarioService: ComentarioService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  this.Comentarios$ = this.comentarioService.buscarComentario(this.id)
+  this.ComentarioForm = this.formBuilder.group({
+    comentario: ['', Validators.maxLength(300)],
+  })
+  }
+
+  gravae(): void{
+    const Comentario = this.ComentarioForm.get('comentario')?.value ?? '';
+    this.Comentarios$ = this.comentarioService.incluirComentario(
+      this.id,
+      Comentario
+    ).pipe(
+      switchMap(()=> this.comentarioService.buscarComentario(this.id)),
+      tap(()=>{
+        this.ComentarioForm.reset();
+      alert('salvo com sucesso');
+      })
+    );
   }
 
 }
